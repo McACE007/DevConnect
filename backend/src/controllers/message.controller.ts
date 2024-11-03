2e37c9033a73038d3c8bb96af3ee7dc008ddfdbd
McACE007 <mrinalace@gmail.com>
2024-11-03 13:35:39 +0530

    update: messages routes and controllers added

---
+++ backend/src/controllers/message.controller.ts
@@ -0,0 +1,108 @@
+import { Request, Response } from "express";
+import db from "../db/prisma.js";
+
+export const sendMessage = async (req: Request, res: Response) => {
+    try {
+       const {message} = req.body;
+       const {id: receiverId} = req.params;
+       const senderId = req.user.id;
+
+       let conversation = await db.conversation.findFirst({
+        where: {
+            participantIds: {
+                hasEvery: [senderId, receiverId],
+            }
+        }
+       });
+
+       if(!conversation){
+        conversation = await db.conversation.create({
+            data:{
+                participantIds: {
+                    set: [senderId, receiverId],
+                }
+            }
+        });
+       }
+
+       const newMessage = await db.message.create({
+        data: {
+            senderId,
+            body: message,
+            conversationId: conversation.id,
+        }
+       });
+
+       if(newMessage){
+        conversation = await db.conversation.update({
+            where: {
+                id: conversation.id,
+            },
+            data: {
+                messages: {
+                    connect: {
+                        id: newMessage.id,
+                    },
+                },
+            },
+        });
+       }
+
+       return res.status(201).json(newMessage);
+    } catch (error: any) {
+       console.error("Error in sendMessage controller: ", error.message);
+       res.status(500).json({error: "Internal Server Error"})
+    }
+}
+
+export const getMessages = async (req: Request, res: Response) => {
+    try {
+       const {id: userToChatId} = req.params;
+       const senderId = req.user.id;
+
+       const conversation = await db.conversation.findFirst({
+        where: {
+            participantIds: {
+                hasEvery: [senderId, userToChatId],
+            },
+        },
+        include: {
+            messages: {
+                orderBy: {
+                    createdAt: "asc"
+                }
+            }
+        }
+       });
+
+       if(!conversation)
+            return res.status(200).json([])
+
+       res.status(200).json(conversation.messages);
+    } catch (error: any) {
+       console.error("Error in getMessages controller: ", error.message);
+       res.status(500).json({error: "Internal Server Error"})
+    }
+}
+
+export const getUsersForSidebar = async (req: Request, res: Response) => {
+    try {
+        const authUserId = req.user.id; 
+        const users = await db.user.findMany({
+            where: {
+                id: {
+                    not: authUserId
+                },
+            },
+            select: {
+                id: true,
+                fullName: true,
+                profilePic: true,
+            }
+        });
+       res.status(200).json(users);
+    } catch (error: any) {
+       console.error("Error in getUsersForSidebar controller: ", error.message);
+       res.status(500).json({error: "Internal Server Error"})
+    }
+}
