88ea9a57d20d4e1125f6fa216660216343b787d4
McACE007 <mrinalace@gmail.com>
2024-11-14 23:17:55 +0530

    update: added config for deveployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoute from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
@@ -9,12 +10,20 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
+
+if (process.env.NODE_ENV !== "development") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("Server is running on PORT:", PORT);
